cea6e18ae9cb84b994fffb1c42cadc70013e1ec7
Steve Carlson <stevengcarlson@gmail.com>
2011-05-19 12:52:49 -0500

    loading gif

--- ScrabbleScrobble/scrabbleLoad.js
+++ ScrabbleScrobble/scrabbleLoad.js
@@ -111,6 +111,13 @@ function onFetchSuccess(response) {
 };
 
 function doFetch() {
+    result.html('<h3>Best Plays</h3><div class="loading"></div>');
+    $('.loading').html('<img src="http://i.imgur.com/TSQWi.gif"></img>');
+    $('.loading img').css('padding-top','10px');
+    $('.loading img').css('width','50px');
+    $('.loading').css('width','120px');
+    $('.loading img').css('height','50px');
+    $('.loading img').css('margin','0 auto');
     rack = $('.play .letter').text();
     while(rack.length < 7){
         rack = rack + '*';
